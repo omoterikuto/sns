ac0ee07138775a36a03b019eeb8aacf1bdc8ed4d
omoterikuto <omoterikuto@gmail.com>
2020-06-13 00:20:29 +0900

    add: create logout function,error message

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -21,7 +21,6 @@ class RegisterController extends Controller
     | provide this functionality without requiring any additional code.
     |
     */
-
   use RegistersUsers;
 
   /**
@@ -50,7 +49,7 @@ class RegisterController extends Controller
   protected function validator(array $data)
   {
     return Validator::make($data, [
-      'name' => ['required', 'string', 'max:255'],
+      'name' => ['required', 'string', 'alpha_num', 'min:3', 'max:16', 'unique:users'],
       'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
       'password' => ['required', 'string', 'min:8', 'confirmed'],
     ]);
@@ -64,10 +63,10 @@ class RegisterController extends Controller
    */
   protected function create(array $data)
   {
-    return Validator::make($data, [
-      'name' => ['required', 'string', 'alpha_num', 'min:3', 'max:16', 'unique:users'],
-      'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-      'password' => ['required', 'string', 'min:8', 'confirmed'],
+    return User::create([
+      'name' => $data['name'],
+      'email' => $data['email'],
+      'password' => Hash::make($data['password']),
     ]);
   }
 }
