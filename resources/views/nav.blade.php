3cab5c626ba91a2727e8ec074a836b18b8c992af
omoterikuto <omoterikuto@gmail.com>
2020-06-14 20:37:04 +0900

    :add create login function, view

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,18 +1,14 @@
 <nav class="navbar navbar-expand navbar-dark blue-gradient">
-
   <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>Memo</a>
-
   <ul class="navbar-nav ml-auto">
-
     @guest
     <li class="nav-item">
       <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
     </li>
     @endguest
-
-    @guest
+    @guest 
     <li class="nav-item">
-      <a class="nav-link" href="">ログイン</a>
+      <a class="nav-link" href="{{ route('login') }}">ログイン</a>
     </li>
     @endguest
       
@@ -21,7 +17,6 @@
       <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
     </li>
     @endauth
-    
     @auth
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
