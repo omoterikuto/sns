d28fd869ce029d3438a1a14ab173da53f2fc6d66
omoterikuto <omoterikuto@gmail.com>
2020-06-15 14:39:20 +0900

    :add create post function

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Article;
 
+use App\Http\Requests\ArticleRequest;
+
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -13,5 +15,16 @@ class ArticleController extends Controller
     $articles = Article::all()->sortByDesc('created_at');
     return view('articles.index', ['articles' => $articles]);
     // return view('articles.index', compact('articles')); でも可能
+  }
+  public function create()
+  {
+    return view('articles.create');
+  }
+  public function store(ArticleRequest $request, Article $article)
+  {
+    $article->fill($request->all());
+    $article->user_id = $request->user()->id;
+    $article->save();
+    return redirect()->route('articles.index');
   }
 }
