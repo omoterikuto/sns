0a8ded95324d252adaaf73b085db861943dd6bee
omoterikuto <omoterikuto@gmail.com>
2020-06-19 22:57:29 +0900

    add: form request,display/register/complement func

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+  protected $fillable = [
+    'name',
+  ];
+  public function getHashtagAttribute(): string
+  {
+    return '#' . $this->name;
+  }
 }
