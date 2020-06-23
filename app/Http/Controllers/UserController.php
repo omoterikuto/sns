04e4294281eb3ae97abd10fb19065fd46d006bc9
Omote Rikuto <52554440+omoterikuto@users.noreply.github.com>
2020-06-23 16:57:45 +0900

    edit: sql number of trials (#13)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,7 @@ class UserController extends Controller
 {
   public function show(string $name)
   {
-    $user = User::where('name', $name)->first();
+    $user = User::where('name', $name)->first()->load(['articles.user', 'articles.likes', 'articles.tags']);
     $articles = $user->articles->sortByDesc('created_at');
     return view('users.show', [
       'user' => $user,
@@ -19,7 +19,7 @@ class UserController extends Controller
 
   public function likes(string $name)
   {
-    $user = User::where('name', $name)->first();
+    $user = User::where('name', $name)->first()->load(['likes.user', 'likes.likes', 'likes.tags']);
     $articles = $user->likes->sortByDesc('created_at');
     return view('users.likes', [
       'user' => $user,
@@ -29,7 +29,7 @@ class UserController extends Controller
 
   public function followings(string $name)
   {
-    $user = User::where('name', $name)->first();
+    $user = User::where('name', $name)->first()->load('followings.followers');
     $followings = $user->followings->sortByDesc('created_at');
     return view('users.followings', [
       'user' => $user,
@@ -39,7 +39,7 @@ class UserController extends Controller
 
   public function followers(string $name)
   {
-    $user = User::where('name', $name)->first();
+    $user = User::where('name', $name)->first()->load('followers.followers');
     $followers = $user->followers->sortByDesc('created_at');
     return view('users.followers', [
       'user' => $user,
