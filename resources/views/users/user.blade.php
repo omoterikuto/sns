0631b72caece29f6d8cda40d4883c545638ea59e
omoterikuto <omoterikuto@gmail.com>
2020-06-27 00:04:11 +0900

    add: user profile message view

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -4,30 +4,34 @@
       <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
         <i class="fas fa-user-circle fa-3x"></i>
       </a>
+      <h2 class="h5 card-title ml-3 mt-2">
+        <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
+          {{ $user->name }}
+        </a>
+      </h2>
       @if( Auth::id() !== $user->id )
         <follow-button
           class="ml-auto"
           :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))'
           :authorized='@json(Auth::check())'
-          endpoint="{{ route('users.follow', ['name' => $user->name]) }}"
-        >
+          endpoint="{{ route('users.follow', ['name' => $user->name]) }}">
         </follow-button>
       @endif
     </div>
-    <h2 class="h5 card-title m-0">
-      <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
-        {{ $user->name }}
+  </div>
+  <div class="card-body">
+  <p>初めまして。よろしくお願いします！初めまして。よろしくお願いします！初めまして。よろしくお願いします！初めまして。よろしくお願いします！初めまして。よろしくお願いします！
+    <a href="">編集</a>
+  </p>
+  </div>
+  <div class="card-body">
+    <div class="card-text">
+      <a href="{{ route('users.followings', ['name' => $user->name]) }}" class="text-muted">
+        {{ $user->count_followings }} フォロー
       </a>
-    </h2>
+      <a href="{{ route('users.followers', ['name' => $user->name]) }}" class="text-muted">
+        {{ $user->count_followers }} フォロワー
+      </a>
+    </div>
   </div>
-<div class="card-body">
-  <div class="card-text">
-    <a href="{{ route('users.followings', ['name' => $user->name]) }}" class="text-muted">
-      {{ $user->count_followings }} フォロー
-    </a>
-    <a href="{{ route('users.followers', ['name' => $user->name]) }}" class="text-muted">
-      {{ $user->count_followers }} フォロワー
-    </a>
-  </div>
-</div>
 </div>
