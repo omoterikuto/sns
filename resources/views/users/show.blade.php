0e787501ffbf402a1978b0e2dbdeb3036d7914a9
Omote Rikuto <52554440+omoterikuto@users.noreply.github.com>
2020-07-06 23:04:50 +0900

    add: post/like tab no page transition (#27)

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,9 +6,25 @@
   @include('nav')
   <div class="container pb-4">
     @include('users.user')
-    @include('users.tabs', ['hasArticles' => true, 'hasLikes' => false])
-    @foreach($articles as $article)
-      @include('articles.card')
-    @endforeach
+    <ul class="nav nav-tabs nav-justified mt-3">
+      <li class="nav-item">
+        <a href="#post" class="nav-link active" data-toggle="tab">記事</a>
+      </li>
+      <li class="nav-item">
+        <a href="#user" class="nav-link" data-toggle="tab">いいね</a>
+      </li>
+    </ul>
+    <div class="tab-content">
+      <div id="post" class="tab-pane active">
+        @foreach($user_articles as $article)
+          @include('articles.card')
+        @endforeach
+      </div>
+      <div id="user" class="tab-pane">
+        @foreach($like_articles as $article)
+          @include('articles.card')
+        @endforeach
+      </div>
+    </div>
   </div>
 @endsection
