bde852f3344ece101ac76121abc189ee88082ce4
omoterikuto <omoterikuto@gmail.com>
2020-07-24 16:24:51 +0900

    edit: UserController name

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ArticleControllerTest extends TestCase
+class UserControllerTest extends TestCase
 {
   use RefreshDatabase;
 
